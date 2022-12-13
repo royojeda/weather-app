8284e3fb3644750d543e4fe76ee72d8780c530cc
Dennis Ojeda <royojeda1@gmail.com>
2022-12-14 05:34:17 +0800

    Add Model#fetchWeatherData

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import "./input.css";
+import Model from "./model";
 
 const root = document.querySelector(".root");
 
 root.className += " bg-black w-screen h-screen";
+
+const model = new Model();
+
+(async () => {
+  await model.fetchWeatherData("san francisco");
+})();
--- src/model.js
+++ src/model.js
@@ -1,3 +1,13 @@
 export default class Model {
-  
+  constructor(apiKey = "c44d173b6a2bd212f94277cedab87736") {
+    this.apiKey = apiKey;
+  }
+
+  async fetchWeatherData(location) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${this.apiKey}`;
+    const response = await fetch(url, { mode: "cors" });
+    const weatherData = await response.json();
+    console.log(weatherData)
+    return weatherData;
+  }
 }
