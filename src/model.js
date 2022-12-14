9093660b4d5a5d4bcfe4649449f9a6f519e74e85
Dennis Ojeda <royojeda1@gmail.com>
2022-12-15 02:38:43 +0800

    Add a processWeatherData method

--- src/model.js
+++ src/model.js
@@ -8,12 +8,39 @@ export default class Model {
   async fetchWeatherData(location) {
     const response = await fetch(this.#createURL(location), { mode: "cors" });
     const weatherData = await response.json();
-    return weatherData;
+    return Model.#processWeatherData(weatherData);
   }
 
   #createURL(location) {
     return `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${
       this.#APIKey
     }`;
+  }
+
+  static #processWeatherData(weatherData) {
+    const processedData = {
+      location: {
+        name: weatherData.name,
+        country: weatherData.sys.country,
+      },
+      condition: {
+        category: weatherData.weather[0].main,
+        description: weatherData.weather[0].description,
+      },
+      cloudiness: weatherData.clouds.all,
+      temperature: weatherData.main.temp,
+      pressure: weatherData.main.pressure,
+      feelsLike: weatherData.main.feels_like,
+      humidity: weatherData.main.humidity,
+      sunrise: weatherData.sys.sunrise,
+      sunset: weatherData.sys.sunset,
+      timeZone: weatherData.timezone,
+      visibility: weatherData.visibility,
+      wind: {
+        direction: weatherData.wind.deg,
+        speed: weatherData.wind.speed,
+      },
+    };
+    return processedData;
   }
 }
