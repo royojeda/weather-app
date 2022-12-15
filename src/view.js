9e0d0c5ca7bc3a0fd9579bc5c180e6fe41f698f3
Dennis Ojeda <royojeda1@gmail.com>
2022-12-16 05:39:28 +0800

    Update the styling of View#showHome

--- src/view.js
+++ src/view.js
@@ -10,11 +10,15 @@ export default class View {
 
   showHome() {
     this.#root.innerHTML = /* html */ `
-      <div class="h-full flex justify-center items-center text-gray-400">
-        <form class="flex items-center gap-2">
-          <input type="text" name="location" id="location" placeholder="Enter a city name..." autofocus required class="outline-none focus:ring-2 focus:ring-indigo-300 border rounded-md shadow-inner p-2 text-center transition placeholder:italic">
-          <button class="bg-indigo-600 text-gray-100 border border-indigo-200 rounded-md shadow shadow-indigo-200 p-2 px-4 transition hover:bg-indigo-500 active:bg-indigo-700">Submit</button>
-        </form>
+      <div class="h-full flex justify-center items-center text-gray-500 p-4">
+        <div class="flex-1 max-w-xs sm:max-w-md flex flex-col gap-10 items-center">
+          <div class="text-6xl font-medium sm:hidden">Weather</div>
+          <div class="w-full text-center text-6xl font-medium hidden sm:block">Get the weather</div>
+          <form class="w-full flex flex-col sm:flex-row gap-2 text-lg">
+            <input type="text" name="location" id="location" placeholder="Enter a city name..." autofocus required class="flex-1 outline-none focus:ring-2 focus:ring-indigo-300 border rounded-md shadow-inner p-2 text-center transition placeholder:italic">
+            <button class="bg-indigo-600 text-gray-100 border border-indigo-200 rounded-md shadow shadow-indigo-200 p-2 px-8 transition hover:bg-indigo-500 active:bg-indigo-700">Go</button>
+          </form>
+        </div>
       </div>
     `;
     this.#form = document.querySelector("form");
