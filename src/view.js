a2838c410c6778e95006a7b2eadeb1fee6a1f4f1
Dennis Ojeda <royojeda1@gmail.com>
2022-12-16 05:02:52 +0800

    Update the placeholder for the location text input

--- src/view.js
+++ src/view.js
@@ -12,7 +12,7 @@ export default class View {
     this.#root.innerHTML = /* html */ `
       <div class="h-full flex justify-center items-center text-gray-400">
         <form class="flex items-center gap-2">
-          <input type="text" name="location" id="location" placeholder="Location" autofocus required class="outline-none focus:ring-2 focus:ring-indigo-300 border rounded-md shadow p-2 text-center transition">
+          <input type="text" name="location" id="location" placeholder="Enter a city name..." autofocus required class="outline-none focus:ring-2 focus:ring-indigo-300 border rounded-md shadow p-2 text-center transition">
           <button class="bg-indigo-600 text-gray-100 border border-indigo-200 rounded-md shadow shadow-indigo-200 p-2 px-4 transition hover:bg-indigo-500 active:bg-indigo-700">Submit</button>
         </form>
       </div>
