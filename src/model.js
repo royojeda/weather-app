2dee3a6fd50afd905338b2ecf3abb667c3a92c48
Dennis Ojeda <royojeda1@gmail.com>
2022-12-17 06:21:19 +0800

    Update processWeatherData to include icon name

--- src/model.js
+++ src/model.js
@@ -29,6 +29,7 @@ export default class Model {
       condition: {
         category: weatherData.weather[0].main,
         description: weatherData.weather[0].description,
+        icon: weatherData.weather[0].icon,
       },
       temperature: weatherData.main.temp,
       feelsLike: weatherData.main.feels_like,
